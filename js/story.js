3eaa6d406b9bdf2362ca43eb15e7f854851304c9
Marco F. Ramos <mfilipe@gmail.com>
2020-04-12 19:17:10 -0400

    1st question|created|tested

--- js/story.js
+++ js/story.js
@@ -1,8 +1,11 @@
 // 1. Declare variables and capture input.
 // ask for adjective
+const adjective = prompt('Please provide an adjective.');
 // ask for verb
 // ask for noun 
 
 // 2. Combine the input with other words to create a story.
 
 // 3. Display the story as a <p> inside the <main> element.
+
+console.log(adjective);
