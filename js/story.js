563129cf5e159c713c2762778dc14e6ad7c41dcb
Marco F. Ramos <mfilipe@gmail.com>
2020-04-12 19:06:19 -0400

    first commit

---
+++ js/story.js
@@ -0,0 +1,8 @@
+// 1. Declare variables and capture input.
+// ask for adjective
+// ask for verb
+// ask for noun 
+
+// 2. Combine the input with other words to create a story.
+
+// 3. Display the story as a <p> inside the <main> element.
